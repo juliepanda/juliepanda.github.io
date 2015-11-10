3b60129c9a77ab5a28caea2bdab2cf8c0598c063
Julie Pan <ycp217@nyu.edu>
2015-11-09 19:28:45 -0800

    draw >1 continuous bezier curve

--- f15-computer-graphics/assignments/bezier/main.js
+++ f15-computer-graphics/assignments/bezier/main.js
@@ -18,11 +18,24 @@ function reverseViewport(p, w, h) {
 	return [(p[0] - (w/2)) * 2/w, (h/2 - p[1]) * 2/h ];
 }
 
+function draw4points(g, step, pc, pt0, pt1, pt2, pt3, width, height) {
+	for (var t = 0; t < 1; t += step) {
+		g.moveTo(pc[0], pc[1]);
+		hx = bezier(pt0[0], pt1[0], pt2[0], pt3[0]);
+		x = hx[0]*(Math.pow(t, 3)) + hx[1]*(Math.pow(t, 2)) + hx[2]*(t) + hx[3];
+		hy = bezier(pt0[1], pt1[1], pt2[1], pt3[1]);
+		y = hy[0]*(Math.pow(t, 3)) + hy[1]*(Math.pow(t, 2)) + hy[2]*(t) + hy[3];
+		pc = viewport([x, y], width, height);
+		g.lineTo(pc[0], pc[1]);
+	}
+	return pc;
+}
+
 var bezpts = [];
 
 var canvas = initCanvas('canvas');
 var g = canvas.getContext('2d');
-var pc, pt0, pt1, pt2, pt3, x, y, hx, hy, width = canvas.width, height = canvas.height, step = 0.05, sq_size = 5;
+var pc, pt0, pt1, pt2, pt3, x, y, hx, hy, width = canvas.width, height = canvas.height, step = 0.05, sqSize = 5, prevSet = 0, first, second, third, fourth;
 canvas.update = function(g) {
 	g.lineWidth = 1;
 	g.strokeStyle = 'black';
@@ -41,55 +54,59 @@ canvas.update = function(g) {
 		var pt = [this.cursor.x, this.cursor.y];
 		if (bezpts.length === 0) bezpts.push(pt);
 		if (pt[0] !== bezpts[bezpts.length-1][0] && pt[1] !== bezpts[bezpts.length-1][1]) bezpts.push(pt);
-
 	}
 
 	if (bezpts.length > 0) {
 		g.lineWidth = 1;
 		g.strokeStyle = 'green';
-		bezpts.map(function(pt) {
-			g.fillRect(pt[0], pt[1], 5, 5);
-		});
+		bezpts.map(function(pt) { g.fillRect(pt[0], pt[1], 5, 5); });
 
 		g.lineWidth = 1;
 		g.strokeStyle = 'red';
 		g.beginPath();
 		if (bezpts.length > 3) {
+			var set = ((bezpts.length - 1) % 3);
+			if (set > 0) {
+				set = prevSet;
+			} else {
+				set = prevSet = ((bezpts.length - 1) / 3) - 1;
+			}
+
 			g.lineWidth = 1;
 			g.strokeStyle = 'green';
-			pt0 = reverseViewport(bezpts[0], width, height);
-			pt1 = reverseViewport(bezpts[1], width, height);
-			pt2 = reverseViewport(bezpts[2], width, height);
-			pt3 = reverseViewport(bezpts[3], width, height);
-			pc = bezpts[0];
-			var pt0c = bezpts[0];
-			var pt1c = bezpts[1];
-			var pt2c = bezpts[2];
-			var pt3c = bezpts[3];
-			g.fillRect(pt0c[0], pt0c[1], sq_size, sq_size);
-			g.fillRect(pt1c[0], pt1c[1], sq_size, sq_size);
-			g.fillRect(pt2c[0], pt2c[1], sq_size, sq_size);
-			g.fillRect(pt3c[0], pt3c[1], sq_size, sq_size);
+
+			for (var a = 0; a < bezpts.length; a++) {
+				g.fillRect(bezpts[a][0], bezpts[a][1], sqSize, sqSize);
+			}
+
 			g.strokeStyle = 'red';
 			g.beginPath();
-			for (var t = 0; t < 1; t += step) {
-				g.moveTo(pc[0], pc[1]);
-				hx = bezier(pt0[0], pt1[0], pt2[0], pt3[0]);
-				x = hx[0]*(Math.pow(t, 3)) + hx[1]*(Math.pow(t, 2)) + hx[2]*(t) + hx[3];
-				hy = bezier(pt0[1], pt1[1], pt2[1], pt3[1]);
-				y = hy[0]*(Math.pow(t, 3)) + hy[1]*(Math.pow(t, 2)) + hy[2]*(t) + hy[3];
-				pc = viewport([x, y], width, height);
-				g.lineTo(pc[0], pc[1]);
-			}
-			var lastPt = bezpts[bezpts.length - 1];
-			if (pc[0] !== lastPt[0] || pc[1] !== lastPt[1]) {
-				g.moveTo(pc[0], pc[1]);
-				g.lineTo(lastPt[0], lastPt[1]);
+			for (var i = 0; i < set + 1; i++) {
+				if (i === 0) {
+					first = 0, second = 1, third = 2, fourth = 3;
+				} else {
+					first = 3*i + 0;
+					second = 3*i + 1;
+					third = 3*i + 2;
+					fourth = 3*i + 3;
+				}
+				pc = bezpts[first];
+				pt0 = reverseViewport(bezpts[first], width, height);
+				pt1 = reverseViewport(bezpts[second], width, height);
+				pt2 = reverseViewport(bezpts[third], width, height);
+				pt3 = reverseViewport(bezpts[fourth], width, height);
+				pc = draw4points(g, step, pc, pt0, pt1, pt2, pt3, width, height);
+				var lastPt = bezpts[fourth];
+				if (pc[0] !== lastPt[0] || pc[1] !== lastPt[1]) {
+					g.moveTo(pc[0], pc[1]);
+					g.lineTo(lastPt[0], lastPt[1]);
+				}
 			}
 			g.stroke();
-
 		}
-		g.stroke();
+
+
 	}
+	g.stroke();
 };
 
