2231c1d8f5e0ac7002bf194e96cd23b4b8e0ae44
Julie Pan <ycp217@nyu.edu>
2015-11-16 22:17:30 -0500

    more parametric shapes

--- f15-computer-graphics/assignments/6.js
+++ f15-computer-graphics/assignments/6.js
@@ -1,29 +1,36 @@
-function ParamObj(u, v) {
+function ParamObj(u, v, r) {
 	if (u != undefined) {
 		this.u = u;
 		this.theta = Math.PI * 2 * this.u;
 	}
-	if (v != undefined) this.v = v;
+	if (v != undefined) {
+		this.v = v;
+		this.phi = Math.PI * this.v - Math.PI / 2;
+	}
+	if (r != undefined) this.r = r;
 }
 
 ParamObj.prototype = {
 	cylinder: function() {
+		'use strict';
 		var x = Math.sin(this.theta);
 		var y = Math.cos(this.theta);
 		var z = 2 * this.v - 1;
 		return [x, y, z];
 	},
 
-	sphere: function(theta, phi) {
-		var x = Math.cos(theta) * Math.cos(phi);
-		var y = Math.cos(phi) * Math.sin(theta);
-		var z = Math.sin(phi);
+	sphere: function() {
+		'use strict';
+		var x = Math.cos(this.theta) * Math.cos(this.phi);
+		var y = Math.cos(this.phi) * Math.sin(this.theta);
+		var z = Math.sin(this.phi);
 		return [x, y ,z]; 
 	},
-	torus: function(theta, phi, r) {
-		var x = (1 + r * Math.cos(phi)) * Math.cos(theta); 
-		var y = (1 + r * Math.cos(phi)) * Math.sin(theta);
-		var z = r * Math.sin(phi);
+	torus: function() {
+		'use strict';
+		var x = (1 + this.r * Math.cos(this.phi)) * Math.cos(this.theta); 
+		var y = (1 + this.r * Math.cos(this.phi)) * Math.sin(this.theta);
+		var z = this.r * Math.sin(this.phi);
 		return [x, y , z];
 	}
 };
@@ -37,31 +44,41 @@ var canvas1 = initCanvas('canvas1');
 var step = 0.01;
 var width = 600;
 var height = 400;
-var sphmat = new Matrix().rotateY(1.2).rotateX(0.5);
-
+var mat = new Matrix().scale(0.3, 0.3, 0.3).rotateY(1.2).rotateX(0.5);
+var stepped = true;
 canvas1.update = function(g) {
 	g.lineWidth = 1;
 	g.strokeStyle = 'green';
 	g.beginPath();
-	for (var t = 0; t < 1; t += step) {
-		var r = 2 * Math.cos(t) + Math.sin(t);
+	for (var u = 0; u < 1; u += step) {
+		for (var v = 0; v < 1; v += step) {
+			var p = new ParamObj(u, v);
+			var obj = p.sphere();
+			var tobj = mat.transform([obj[0], obj[1], obj[2]]);
+			var coords = viewport([tobj[0], tobj[1]], width, height);
+			if (stepped) {
+				g.moveTo(coords[0], coords[1]);
+				stepped = false;
+			} else {
+				g.lineTo(coords[0], coords[1]);
+				stepped = true;
+			}
+		}
 	}
 	g.stroke();
 };
 
 var canvas2 = initCanvas('canvas2');
-var cymat = new Matrix().scale(0.3, 0.3, 0.3).rotateY(1.2).rotateX(0.5);
-var stepped = true;
 canvas2.update = function(g) {
 	g.lineWidth = 1;
 	g.strokeStyle = 'green';
 	g.beginPath();
 	for (var u = 0; u < 1; u += step) {
 		for (var v = 0; v < 1; v += step) {
-			var cy = new ParamObj(u, v);
-			var obj = cy.cylinder();
-			var cyc = cymat.transform([obj[0], obj[1], obj[2]]);
-			var coords = viewport([cyc[0], cyc[1]], width, height);
+			var p = new ParamObj(u, v);
+			var obj = p.cylinder();
+			var tobj = mat.transform([obj[0], obj[1], obj[2]]);
+			var coords = viewport([tobj[0], tobj[1]], width, height);
 			if (stepped) {
 				g.moveTo(coords[0], coords[1]);
 				stepped = false;
@@ -75,22 +92,26 @@ canvas2.update = function(g) {
 };
 
 
-var to = new ParamObj(1, 1);
 var canvas3 = initCanvas('canvas3');
-var tomat = new Matrix().scale(0.3, 0.3, 0.3).rotateY(1.2).rotateX(0.5);
+var r = 0.5;
 canvas3.update = function(g) {
 	g.lineWidth = 1;
 	g.strokeStyle = 'green';
 	g.beginPath();
-	for (var theta = 0, phi = 0; theta < to.theta - step || phi < to.phi - step; theta+=step, phi+=step) {
-		var obj = to.torus(theta, phi, 0.2);
-		var toc = tomat.transform([obj[0], obj[1], obj[2]]);
-		var coords = viewport([toc[0], toc[1]], width, height);
-		g.moveTo(coords[0], coords[1]);
-		obj = to.torus(theta + step, phi + step, 0.2);
-		toc = tomat.transform([obj[0], obj[1], obj[2]]);
-		coords = viewport([toc[0], toc[1]], width, height);
-		g.lineTo(coords[0], coords[1]);
+	for (var u = 0; u < 1; u += step) {
+		for (var v = 0; v < 1; v += step) {
+			var p = new ParamObj(u, v, r);
+			var obj = p.torus();
+			var tobj = mat.transform([obj[0], obj[1], obj[2]]);
+			var coords = viewport([tobj[0], tobj[1]], width, height);
+			if (stepped) {
+				g.moveTo(coords[0], coords[1]);
+				stepped = false;
+			} else {
+				g.lineTo(coords[0], coords[1]);
+				stepped = true;
+			}
+		}
 	}
 	g.stroke();
 };
